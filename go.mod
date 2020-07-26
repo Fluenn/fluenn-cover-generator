a69bfb106d6a985f7c41e48877b7c1d6bc9a57fd
Manuel Vargas <im@devmanny.io>
2020-07-26 15:35:39 -0500

    source

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/fluenn/fluenn-cover-generator
 
 go 1.14
+
+require (
+	github.com/oliamb/cutter v0.2.2
+	github.com/valyala/fastrand v1.0.0
+)
---
+++ main.go
@@ -0,0 +1,136 @@
+package generate
+
+import (
+	"fmt"
+	"image"
+
+	_ "image/gif"
+	"image/jpeg"
+
+	_ "image/jpeg"
+
+	_ "image/png"
+	"os"
+
+	"github.com/oliamb/cutter"
+	"github.com/valyala/fastrand"
+)
+
+func getImageSize(imgPath string) *image.Config {
+
+	fmt.Println(imgPath)
+	file, err := os.Open(imgPath)
+	defer file.Close()
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	image, _, err := image.DecodeConfig(file)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	return &image
+
+}
+
+func getCropCoordinate(originalWidth, originalHeight, cropWidth, cropHeight uint32) (uint, uint) {
+	var minX uint32 = 0
+	var minY uint32 = 0
+	var maxX uint32 = originalWidth - cropWidth
+	var maxY uint32 = originalHeight - cropHeight
+
+	x := fastrand.Uint32n(maxX-minX) + minX
+	y := fastrand.Uint32n(maxY-minY) + minY
+
+	return uint(x), uint(y)
+}
+
+func cropImage(path string, width uint, height uint, x uint, y uint) (image.Image, error) {
+
+	fmt.Println(path, width, height, x, y)
+
+	file, err := os.Open(path)
+	defer file.Close()
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	croppedImg, err := cutter.Crop(img, cutter.Config{
+		Width:  int(width),
+		Height: int(height),
+		Anchor: image.Point{int(x), int(y)},
+		Mode:   cutter.TopLeft, // optional, default value
+	})
+
+	return croppedImg, err
+
+}
+
+func SaveImage(img image.Image, path string) {
+
+	f, err := os.Create(path)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	opt := jpeg.Options{
+		Quality: 90,
+	}
+
+	err = jpeg.Encode(f, img, &opt)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+type CropConfig struct {
+	width  int
+	height int
+	x      int
+	y      int
+}
+
+func GenerateFrom(imgPath string) image.Image {
+
+	imageConfig := getImageSize(imgPath)
+
+	var cropWidth uint = 600
+	var cropHeight uint = 350
+	originalWidth := imageConfig.Width
+	originalHeight := imageConfig.Height
+
+	x, y := getCropCoordinate(
+		uint32(originalWidth),
+		uint32(originalHeight),
+		uint32(cropWidth),
+		uint32(cropHeight),
+	)
+
+	img, err := cropImage(
+		imgPath,
+		cropWidth,
+		cropHeight,
+		x,
+		y,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return img
+}
